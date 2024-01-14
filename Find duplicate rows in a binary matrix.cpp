56fe4465e23a639d6eee1ac66fd8ca43c58b5f05
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-01-14 18:27:00 +0530

    Create Find duplicate rows in a binary matrix.cpp
    Given a boolean matrix of size RxC where each cell contains either 0 or 1, find the row numbers (0-based) of row which already exists or are repeated.
    
    Example 1:
    
    Input:
    R = 2, C = 2
    matrix[][] = {{1, 0},
                {1, 0}}
    Output: 
    1
    Explanation:
    Row 1 is duplicate of Row 0.

---
+++ Find duplicate rows in a binary matrix.cpp
@@ -0,0 +1,21 @@
+vector<int> repeatedRows(vector<vector<int>> &matrix, int M, int N) 
+    { 
+        // Your code here
+       vector<int> result;
+        unordered_set<string> set;
+
+        for (int i = 0; i < M; i++) {
+            string row;
+            for (int j = 0; j < N; j++) {
+                row += to_string(matrix[i][j]);
+            }
+
+            if (set.count(row) > 0) {
+                result.push_back(i);
+            } else {
+                set.insert(row);
+            }
+        }
+
+        return result;
+    } 
