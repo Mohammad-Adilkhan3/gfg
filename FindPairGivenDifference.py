858bc73ce7d5ceb2ae21c0cf205e4c367d9332ab
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-15 07:39:19 +0530

    Create FindPairGivenDifference.py
    Given an array Arr[] of size L and a number N, you need to write a program to find if there exists a pair of elements in the array whose difference is N.

---
+++ FindPairGivenDifference.py
@@ -0,0 +1,11 @@
+def findPair(self, arr, L,N):
+        #code here
+        arr.sort();i=0;j=1
+        while j<L:
+            if j>i and arr[j]-arr[i]==N:
+                return True
+            elif arr[j]-arr[i]>N:
+                i+=1
+            else:
+                j+=1
+        return False
