d6edda09a6bc10af2653128a24d8614fa912559d
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-05-04 21:54:23 +0530

    Create printLL.cpp

---
+++ printLL.cpp
@@ -0,0 +1,9 @@
+void display(Node *head)
+    {
+      //your code goes here
+      Node* temp = head;
+      while(temp!=NULL) {
+          cout << temp->data << " ";
+          temp = temp->next;
+      }
+    }
