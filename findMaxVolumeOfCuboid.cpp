30d369b72c0a0714fb7217493ebd1caa012f9aa8
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-06-19 22:32:13 +0530

    Create findMaxVolumeOfCuboid.cpp

---
+++ findMaxVolumeOfCuboid.cpp
@@ -0,0 +1,8 @@
+double maxVolume(double perimeter, double area) {
+        double ans =
+            (pow((perimeter - (sqrt(pow(perimeter, 2) - (24 * area)))) / 12, 2) *
+             ((perimeter / 4) -
+              (2 * ((perimeter - (sqrt(pow(perimeter, 2) - (24 * area)))) / 12))));
+
+        return ans;
+    }
