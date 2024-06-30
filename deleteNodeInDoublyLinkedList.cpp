f0a74f666f8e9c8e46076bd6ad6ff1a2a778acf4
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-06-30 23:20:56 +0530

    Create deleteNodeInDoublyLinkedList.cpp

---
+++ deleteNodeInDoublyLinkedList.cpp
@@ -0,0 +1,13 @@
+Node* deleteNode(Node* head, int x) {
+        // Your code here
+        if(x==1)return head->next;
+        Node *temp=head;
+        int count=1;
+        while(count<x){
+            temp=temp->next;
+            count++;
+        }
+        temp->prev->next=temp->next;
+        if(temp->next)temp->next->prev=temp->prev;
+        return head;
+    }
