782f14df95a1c5a5049d4fceaab0ecdbcec63b97
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-09-24 23:43:38 +0530

    Update nonRepeatingElement.py

--- nonRepeatingElement.py
+++ nonRepeatingElement.py
@@ -2,12 +2,11 @@ def firstNonRepeating(self, arr):
         # Complete the function
         d={}
         for i in arr:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        # print(d)
-        for i in d:
-            if d[i]==1:
-                return i
-        return 0
+           if i not in d:
+               d[i]=1
+           else:
+               d[i]+=1
+        for i in arr:
+           if d[i]==1:
+               return i
+        return 0    
