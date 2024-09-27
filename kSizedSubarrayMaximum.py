748c7fe50cf0097c7b970f4597a09caba4f8f2aa
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-09-27 23:27:00 +0530

    Create kSizedSubarrayMaximum.py

---
+++ kSizedSubarrayMaximum.py
@@ -0,0 +1,16 @@
+def max_of_subarrays(self,k,arr):
+        import heapq
+        #code here
+        if not arr or k == 0:
+            return []
+        n = len(arr)
+        result = []
+        max_heap = []
+        for i in range(n):
+            heapq.heappush(max_heap, (-arr[i], i))
+            while max_heap[0][1] <= i - k:
+                heapq.heappop(max_heap)
+            if i >= k - 1:
+                result.append(-max_heap[0][0])  
+    
+        return result
