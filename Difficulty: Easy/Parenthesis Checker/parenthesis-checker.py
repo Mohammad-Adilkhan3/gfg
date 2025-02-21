64d8e79dedf6571c1981e273706cf10327a65c56
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-02-21 22:59:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.py
@@ -0,0 +1,51 @@
+
+class Solution:
+    def isBalanced(self, x):
+        # code here
+        stack=[]
+        matching_brackets = {')': '(', '}': '{', ']': '['}
+        for char in x:
+            if char in '({[':
+                stack.append(char)
+            elif char in ')}]':
+                if not stack or stack[-1] != matching_brackets[char]:
+                    return False
+                stack.pop()
+        return len(stack) == 0
+
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        s = str(input())
+        obj = Solution()
+        if obj.isBalanced(s):
+            print("true")
+        else:
+            print("false")
+        print("~")
+
+# } Driver Code Ends
