fd6a67fc7aab68bd74de27d32f2315598982900e
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-05-24 17:27:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of all substrings of a number/sum-of-all-substrings-of-a-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumSubstrings(self,s):
+        # code here
+        sub=[]
+        n=len(s)
+        for i in range(n):
+            for j in range(i+1,n+1):
+                sub.append(s[i:j])
+        count=0
+        for i in sub:
+            count+=int(i)
+        return count  
+                
