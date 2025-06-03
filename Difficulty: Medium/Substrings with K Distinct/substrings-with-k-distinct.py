8872b4abdc4da1773d279c236758c27ad70fda50
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-06-03 15:04:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.py
@@ -0,0 +1,31 @@
+class Solution:
+    def substrk(self,s,l,k):
+        f=0
+        dct={}
+        i,j=0,0
+        cnt=0
+        for i in range(0,l):
+            while j<l and len(dct)<k:
+                if s[j] not in dct:
+                    dct[s[j]]=1
+                    
+                else:
+                    dct[s[j]]+=1
+                    
+                j+=1
+            if len(dct)>=k:
+                cnt+=l-j+1
+            
+            dct[s[i]]-=1
+            if dct[s[i]]==0:
+                del dct[s[i]]
+            
+        return cnt
+            
+                
+                    
+        
+    def countSubstr (self,s, k):
+        l=len(s)
+        return self.substrk(s,l,k)-self.substrk(s,l,k+1)
+
