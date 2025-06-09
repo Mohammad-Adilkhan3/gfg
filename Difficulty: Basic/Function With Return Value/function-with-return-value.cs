58ca026953cef257695e849ae2e6d685ecdae9dd
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-06-09 11:35:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Return Value/function-with-return-value.cs
@@ -0,0 +1,9 @@
+// User function Template for C#
+
+class Solution {
+    // Complete the function below
+    public int returnValueFunction(int n) {
+        // Your code here
+        return n*2;
+    }
+}
