af98bc27d8c0638d3d74e699a04d8146b4911e3b
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-06-09 11:33:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+
+########### Write your code below ###############
+
+# Write Code to Swap
+
+########### Write your code above ###############
+a=a^b
+b=a^b
+a=a^b
+
+print(a, b)
