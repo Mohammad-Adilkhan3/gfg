d666c4134658b7f926c8c0b6ef257729732a8d8b
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-06-27 10:48:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Mobile numeric keypad/mobile-numeric-keypad.py
@@ -0,0 +1,34 @@
+class Solution:
+	def getCount(self, n):
+		# code here
+		from itertools import combinations
+        if n==1:
+            return 10
+        moves = {
+        '0': ['0', '8'],
+        '1': ['1', '2', '4'],
+        '2': ['2', '1', '3', '5'],
+        '3': ['3', '2', '6'],
+        '4': ['4', '1', '5', '7'],
+        '5': ['5', '2', '4', '6', '8'],
+        '6': ['6', '3', '5', '9'],
+        '7': ['7', '4', '8'],
+        '8': ['8', '5', '7', '9', '0'],
+        '9': ['9', '6', '8']
+        }
+        
+        
+        dp = [[0] * 10 for _ in range(n + 1)]
+        
+        for j in range(10):
+            dp[1][j] = 1
+        
+        for i in range(2, n + 1):
+            for j in range(10):
+                for move in moves[str(j)]:
+                    dp[i][j] += dp[i - 1][int(move)]
+        
+        result = sum(dp[n][j] for j in range(10))
+        
+        return result
+
