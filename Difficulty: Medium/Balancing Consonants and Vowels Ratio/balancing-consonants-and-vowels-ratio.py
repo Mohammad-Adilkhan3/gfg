012d880fd9a84560dc35b96c43ef387324483f87
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-08-01 22:43:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Balancing Consonants and Vowels Ratio/balancing-consonants-and-vowels-ratio.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countBalanced(self, arr):
+        # code here
+        def is_vowel(ch):
+            return ch in 'aeiou'
+
+        prefix_diff_count = {0: 1}
+        total = 0
+        vowel_count = consonant_count = 0
+
+        for word in arr:
+            for ch in word:
+                if is_vowel(ch):
+                    vowel_count += 1
+                else:
+                    consonant_count += 1
+
+            diff = vowel_count - consonant_count
+            total += prefix_diff_count.get(diff, 0)
+            prefix_diff_count[diff] = prefix_diff_count.get(diff, 0) + 1
+
+        return total
+        
