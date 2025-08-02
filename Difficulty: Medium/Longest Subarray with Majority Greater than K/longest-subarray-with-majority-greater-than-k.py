6c6e8c58a58742176791293782c7723b1304100e
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-08-02 23:36:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
@@ -1,46 +1,15 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-
-#User function Template for python3
 class Solution:
     def longestSubarray(self, arr, k):
         # Code Here
-        n = len(arr)
-        transformed = [1 if x > k else -1 for x in arr]
-    
-        prefix_sum = 0
-        max_len = 0
-        first_occurrence = {0: -1}  # prefix_sum : earliest index
-    
-        for i in range(n):
-            prefix_sum += transformed[i]
-    
-            if prefix_sum > 0:
-                max_len = i + 1
-            elif prefix_sum - 1 in first_occurrence:
-                max_len = max(max_len, i - first_occurrence[prefix_sum - 1])
-    
-            if prefix_sum not in first_occurrence:
-                first_occurrence[prefix_sum] = i
-    
-        return max_len
-
-
-#{ 
- # Driver Code Starts.
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        
-        arr = [int(x) for x in input().strip().split()]
-        k = int(input())
-        
-        ob = Solution()
-        print(ob.longestSubarray(arr, k))
-        print("~")
-        t -= 1
-# } Driver Code Ends
+        from itertools import accumulate
+        arr = [-1 if e <= k else 1 for e in arr]
+        acc = accumulate(arr)
+        m, running, ans = {}, 0, 0
+        for i, e in enumerate(acc):
+            if e > 0: # e is already > 0, 0..i is good
+                ans = max(ans, i+1) 
+            if e not in m:
+                m[e] = i
+            if e-1 in m:
+                ans = max(ans, i-m[e-1])
+        return ans
