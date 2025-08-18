1dd10bcb8261f0733200da30438f5e948228a1b7
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-08-18 23:03:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.py
+++ Difficulty: Medium/Find H-Index/find-hindex.py
@@ -20,21 +20,3 @@ class Solution:
         return 0
 
 
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
-# Main
-if __name__ == '__main__':
-    t = int(input())
-    while t > 0:
-        t -= 1
-        arr = [int(x) for x in input().strip().split()]
-
-        ob = Solution()
-        result = ob.hIndex(arr)
-
-        print(result)
-        print("~")
-
-# } Driver Code Ends
