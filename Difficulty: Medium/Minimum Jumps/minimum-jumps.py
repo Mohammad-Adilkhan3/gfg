907d456aa3d1d400ee24e58851918f8364479100
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-09-11 09:48:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.py
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.py
@@ -26,16 +26,3 @@ class Solution:
 
 
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-if __name__ == '__main__':
-    T = int(input())
-    for i in range(T):
-        # n = int(input())
-        Arr = [int(x) for x in input().split()]
-        ob = Solution()
-        ans = ob.minJumps(Arr)
-        print(ans)
-        print("~")
-# } Driver Code Ends
