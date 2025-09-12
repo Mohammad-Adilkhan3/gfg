6eb6ced7644d1e373622f1ced17ece66b1f177a7
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-09-12 14:58:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+    def getMinDiff(self, arr,k):
+        # code here
+        n=len(arr)
+        arr.sort()
+        res=max(arr)-min(arr)
+        for i in range(1,n):
+            if arr[i]>k:
+                mx=max(arr[i-1]+k,arr[n-1]-k)
+                mn=min(arr[0]+k,arr[i]-k)
+                res=(min(res,mx-mn))
+        return res
+
+
+
