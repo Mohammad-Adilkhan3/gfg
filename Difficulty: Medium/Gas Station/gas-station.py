922bea280b9e9159d9f30455c8b4d36a80557004
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-09-14 20:03:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.py
+++ Difficulty: Medium/Gas Station/gas-station.py
@@ -1,36 +1,14 @@
 class Solution:
     def startStation(self, gas, cost):
-        # Your code here
-        total_gas, total_cost, start, balance = 0, 0, 0, 0
-
-        for i in range(len(gas)):
-            total_gas += gas[i]
-            total_cost += cost[i]
-            balance += gas[i] - cost[i]
-    
-            # If balance goes negative, reset start station
-            if balance < 0:
+        #  code here
+        if sum(gas) < sum(cost):
+            return -1
+        
+        start = 0
+        tank = 0
+        for i in range (len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
                 start = i + 1
-                balance = 0
-    
-        return start if total_gas >= total_cost else -1
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import io
-import sys
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        gas = list(map(int, input().strip().split()))
-        cost = list(map(int, input().strip().split()))
-        ob = Solution()
-        ans = ob.startStation(gas, cost)
-        print(ans)
-        print("~")
-
-# } Driver Code Ends
+                tank = 0
+        return start
