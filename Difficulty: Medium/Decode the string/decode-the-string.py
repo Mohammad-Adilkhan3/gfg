e94b2b934c6336d1577fda10a71de2176f16e73a
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-09-17 21:51:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.py
+++ Difficulty: Medium/Decode the string/decode-the-string.py
@@ -24,17 +24,3 @@ class Solution:
         return curr
 
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        s = input()
-
-        ob = Solution()
-        print(ob.decodedString(s))
-        print("~")
-
-# } Driver Code Ends
