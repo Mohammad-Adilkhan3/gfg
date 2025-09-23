9a4f4622fddcbc098301db4bdec8e4c2d209b12d
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-09-23 22:13:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Queue Reversal/queue-reversal.py
+++ Difficulty: Easy/Queue Reversal/queue-reversal.py
@@ -2,12 +2,5 @@ class Solution:
     #Function to reverse the queue.
     def reverseQueue(self, q):
         # code here
-        return rev(q)
-def rev(q):
-    if q.empty():
-        return q
-    ele=q.get()
-    r=rev(q)
-    r.put(ele)
-    return r
+        return q.reverse()
         
