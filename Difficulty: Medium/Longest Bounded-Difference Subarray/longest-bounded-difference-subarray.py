7778835c44c1cd8c508c9a83df8822b146d7cb24
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-09-28 16:42:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.py
+++ Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.py
@@ -25,19 +25,3 @@ class Solution:
         
 
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-if __name__ == '__main__':
-    tc = int(input())
-    while tc > 0:
-        arr = list(map(int, input().strip().split()))
-        k = int(input().strip())
-        ob = Solution()
-        ans = ob.longestSubarray(arr, k)
-
-        print(" ".join(map(str, ans)))
-        tc -= 1
-        print("~")
-
-# } Driver Code Ends
