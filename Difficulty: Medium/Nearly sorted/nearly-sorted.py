65bd5f02c7834ac196dc428c13e4591f67d6f269
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-10-22 11:07:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nearly sorted/nearly-sorted.py
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.py
@@ -3,24 +3,16 @@
 class Solution:
     def nearlySorted(self, arr, k):
         #code
-        arr.sort()
-        return arr
+        import heapq
+        heap = arr[:k + 1]
+        heapq.heapify(heap)
+        target_idx = 0
+        for i in range(k + 1, len(arr)):
+            arr[target_idx] = heapq.heappop(heap)
+            target_idx += 1
+            heapq.heappush(heap, arr[i])
+        while heap:
+            arr[target_idx] = heapq.heappop(heap)
+            target_idx += 1
 
 
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-# Position this line where user code will be pasted.
-# Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        arr = list(map(int, input().split()))
-        k = int(input())
-        ob = Solution()
-        ob.nearlySorted(arr, k)
-        print(*arr)
-        # print("~")
-        t -= 1
-# } Driver Code Ends
