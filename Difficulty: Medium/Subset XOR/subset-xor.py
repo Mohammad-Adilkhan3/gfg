74278bf019bcb8cc722f731a42cb25b016da591b
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-11-28 15:58:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset XOR/subset-xor.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsetXOR(self, n : int):
+        # code here
+        x = 0
+        for i in range(1, n + 1):
+            x ^= i
+        if x == n:
+            return [i for i in range(1, n + 1)]
+            
+        k = x ^ n
+        
+        if 1 <= k <= n:
+            return [i for i in range(1, n + 1) if i != k]
+            
+        return [i for i in range(1, n)]
+        
