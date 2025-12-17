2db127a0c2d40c770807e64cf56b4bdadc8fced8
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-12-17 22:58:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
@@ -1,7 +1,7 @@
 class Solution:
-	def mergeOverlap(self, arr):
-		#Code here
-		arr.sort(key=lambda x: x[0])
+    def mergeOverlap(self, arr):
+        #Code here
+        arr.sort(key=lambda x: x[0])
         # Step 2: Merge intervals
         merged = []
         for interval in arr:
@@ -13,30 +13,6 @@ class Solution:
                 merged[-1][1] = max(merged[-1][1], interval[1])
         
         return merged
-		
+        
 
 
-#{ 
- # Driver Code Starts
-if __name__ == '__main__':
-    T = int(input())
-    for i in range(T):
-        n = int(input())
-        # a = list(map(int, input().strip().split()))
-        arr = []
-        # j = 0
-        for i in range(n):
-            a = list(map(int, input().strip().split()))
-            x = a[0]
-            # j += 1
-            y = a[1]
-            # j += 1
-            arr.append([x, y])
-        obj = Solution()
-        ans = obj.mergeOverlap(arr)
-        for i in ans:
-            for j in i:
-                print(j, end=" ")
-        print()
-
-# } Driver Code Ends
