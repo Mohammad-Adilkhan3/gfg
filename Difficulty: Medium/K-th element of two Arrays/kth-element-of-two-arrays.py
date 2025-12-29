dff0d12bc957f17ceb7412f9553f8360f03bdcc6
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2025-12-29 21:57:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
@@ -4,33 +4,8 @@
 class Solution:
 
     def kthElement(self, a, b, k):
-        res=a+b
-        res.sort()
+        res=sorted(a+b)
         return res[k-1]
 
 
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-
-def main():
-
-    T = int(input())
-
-    while (T > 0):
-
-        k = int(input())
-        a = [int(x) for x in input().strip().split()]
-        b = [int(x) for x in input().strip().split()]
-        ob = Solution()
-        print(ob.kthElement(a, b, k))
-        print("~")
-        T -= 1
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
