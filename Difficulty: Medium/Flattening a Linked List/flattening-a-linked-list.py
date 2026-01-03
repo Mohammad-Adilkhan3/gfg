5102da71e3b7e68e8a7e6f096786e47b8e681914
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-01-03 23:27:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.py
@@ -0,0 +1,33 @@
+'''
+class Node:
+    def __init__(self, d):
+        self.data=d
+        self.next=None
+        self.bottom=None
+        
+'''
+
+class Solution:
+    def flatten(self, root):
+        # code here
+        if not root or not root.next:
+            return root
+        root.next = self.flatten(root.next)
+        root = self.merge(root, root.next)
+        return root
+    def merge(self, a, b):
+        dummy = Node(0)
+        temp = dummy
+        while a and b:
+            if a.data < b.data:
+                temp.bottom = a
+                a = a.bottom
+            else:
+                temp.bottom = b
+                b = b.bottom
+            temp = temp.bottom
+        if a:
+            temp.bottom = a
+        if b:
+            temp.bottom = b
+        return dummy.bottom
