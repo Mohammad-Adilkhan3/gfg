189315dbb0cf0b597d9f27b98e822382d306c9ef
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-01-21 12:46:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.py
+++ Difficulty: Medium/Stock span problem/stock-span-problem.py
@@ -1,11 +1,6 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
 class Solution:
     def calculateSpan(self, arr):
-        #write code here
+        # code here
         n = len(arr)
         stack = []  
         res = [0] * n  
@@ -20,18 +15,4 @@ class Solution:
         
         return res
         
-
-#{ 
- # Driver Code Starts.
-#Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        ans = ob.calculateSpan(arr)
-        print(*ans)
-        print("~")
-        t -= 1
-# } Driver Code Ends
+        
