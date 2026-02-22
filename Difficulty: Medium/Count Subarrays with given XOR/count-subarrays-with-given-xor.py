f8c06880fb66f867f69bce7ace120fea27872935
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-02-22 12:57:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.py
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.py
@@ -1,31 +1,12 @@
 class Solution:
     def subarrayXor(self, arr, k):
         # code here
-        prefix_xor = 0
-        count = 0
-        freq_map = {0: 1}  # To handle cases where the prefix XOR itself equals k
-        for num in arr:
-            prefix_xor ^= num
-            # Check if prefix_xor ^ k exists in the hash map
+        prefix_xor=0
+        cnt=0
+        freq_map={0:1}
+        for i in arr:
+            prefix_xor ^= i
             if prefix_xor ^ k in freq_map:
-                count += freq_map[prefix_xor ^ k]
-            # Update the frequency map
-            freq_map[prefix_xor] = freq_map.get(prefix_xor, 0) + 1
-        
-        return count
-
-
-#{ 
- # Driver Code Starts
-if __name__ == "__main__":
-    tc = int(input())
-
-    for _ in range(tc):
-        arr = list(map(int, input().split()))
-        k = int(input())
-
-        obj = Solution()
-        print(obj.subarrayXor(arr, k))
-        print("~")
-
-# } Driver Code Ends
+                cnt+=freq_map[prefix_xor ^ k]
+            freq_map[prefix_xor]=freq_map.get(prefix_xor,0)+1
+        return cnt
