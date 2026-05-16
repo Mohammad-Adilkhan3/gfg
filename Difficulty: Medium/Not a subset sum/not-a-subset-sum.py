c14fdcd0dbca20bc68c976c0511c6c5fcbfc79d0
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-05-16 23:15:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Not a subset sum/not-a-subset-sum.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+class Solution:
+    def findSmallest(self, arr):
+        # code here
+        arr.sort()
+        res = 1
+        for num in arr:
+            if num > res:
+                break
+            res += num
+        return res
+
