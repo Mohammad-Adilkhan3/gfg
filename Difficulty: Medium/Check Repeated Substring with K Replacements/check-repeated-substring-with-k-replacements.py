0e75adec827266d1068079b69847d8c4b4bc063c
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-06-12 23:08:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Repeated Substring with K Replacements/check-repeated-substring-with-k-replacements.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kSubstr(self, s: str, k: int) -> bool:
+        # code here
+        n = len(s)
+
+        if n % k != 0:
+            return False
+
+        freq = {}
+        blocks = n // k
+
+        for i in range(0, n, k):
+            part = s[i:i + k]
+            freq[part] = freq.get(part, 0) + 1
+
+        return max(freq.values()) >= blocks - 1
