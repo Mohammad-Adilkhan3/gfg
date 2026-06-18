7918640d6a65aa3ffa363e0913e093d801d03cee
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-06-18 23:31:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findCoverage(self, mat):
+        # code here
+        ret=0
+        for rw in mat:
+            zer=0
+            one=False
+            for x in rw:
+                if x==1:
+                    one=True
+                    ret+=zer
+                    zer=0
+                else:
+                    ret+=one
+                    zer+=1
+        for cl in zip(*mat):
+            zer=0
+            one=False
+            for x in cl:
+                if x==1:
+                    one=True
+                    ret+=zer
+                    zer=0
+                else:
+                    ret+=one
+                    zer+=1
+        return ret
