5926e0def08c7e2da90307e056763d02a61e80d2
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2026-08-08 00:24:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countFriendsPairings(self, n: int) -> int:
+        # code here 
+        from functools import cache
+        @cache
+        def dp(ix=n):
+            if ix<=0:
+                return 1
+            return dp(ix-1)+(ix-1)*dp(ix-2)
+        return dp()
+
